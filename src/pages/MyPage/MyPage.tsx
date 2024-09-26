6fc7381ef935e90d4042a84e7925c9fd62658a46
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-09-26 17:06:51 +0900

    Design: change subtitle

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,13 +1,50 @@
+import React, { useEffect } from 'react';
 import * as S from './MyPageStyle';
-import Header from '../../components/Header/Header';
-import TabBar from '../../components/TabBar/TabBar';
 
 const MyPage: React.FC = () => {
+  const cards = [
+    '카드 1',
+    '카드 2',
+    '카드 3',
+    '카드 4',
+    '카드 5',
+    '카드 6',
+    '카드 7',
+  ];
+
+  const duplicatedCards = [...cards, ...cards]; // 카드 배열을 복제
+
+  useEffect(() => {
+    // 스크롤 방지
+    const preventScroll = (e: WheelEvent | TouchEvent) => {
+      e.preventDefault();
+    };
+
+    window.addEventListener('wheel', preventScroll, { passive: false });
+    window.addEventListener('touchmove', preventScroll, { passive: false });
+
+    return () => {
+      window.removeEventListener('wheel', preventScroll);
+      window.removeEventListener('touchmove', preventScroll);
+    };
+  }, []);
+
   return (
     <S.Container>
-      <Header />
-      <p>My Page</p>
-      <TabBar />
+      <S.AnimationLayout>
+        {duplicatedCards.map((card, index) => (
+          <S.FlowBox key={index}>
+            <p>{card}</p>
+          </S.FlowBox>
+        ))}
+      </S.AnimationLayout>
+      <S.AnimationLayoutReverse>
+        {duplicatedCards.map((card, index) => (
+          <S.FlowBox key={index}>
+            <p>{card}</p>
+          </S.FlowBox>
+        ))}
+      </S.AnimationLayoutReverse>
     </S.Container>
   );
 };
--- src/pages/MyPage/MyPageStyle.ts
+++ src/pages/MyPage/MyPageStyle.ts
@@ -1,15 +1,51 @@
-import { styled } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const flowing = keyframes`
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+  100% {
+    transform: translate3d(-100%, 0, 0); 
+  }
+`;
+
+const flowingReverse = keyframes`
+  0% {
+    transform: translate3d(-100%, 0, 0);
+  }
+  100% {
+    transform: translate3d(0, 0, 0); 
+  }
+`;
 
 // 스타일드 컴포넌트 정의
 export const Container = styled.div`
   box-sizing: border-box;
-  padding: 54px 20px 80px 20px;
+  padding: 100px 0;
   width: 100vw;
   height: 100vh;
   overflow: hidden;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  row-gap: 40px;
+`;
+
+export const AnimationLayout = styled.div`
+  /* border: 1px solid red; */
+  width: 100%;
+  display: flex;
+  animation: ${flowing} 20s linear infinite;
+`;
+
+export const AnimationLayoutReverse = styled.div`
+  /* border: 1px solid red; */
+  width: 100%;
+  display: flex;
+  animation: ${flowingReverse} 20s linear infinite;
+`;
+
+export const FlowBox = styled.div`
+  border: 1px solid red;
+  width: 300px; // 고정된 너비
+  height: 200px;
+  flex-shrink: 0; // 카드가 줄어들지 않도록 설정
 `;
